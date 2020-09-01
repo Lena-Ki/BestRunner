9b87bdbb21b6dc2b6c23ae87416682478643a375
Helen Kim <lenahomerus@gmail.com>
2020-09-01 14:06:30 +0300

    Updates

---
+++ precache-manifest.4b556467796f700461adc4054d50aa1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3ebcc7fb9764f051d566fcd7f8101b",
+    "url": "/BestRunner/index.html"
+  },
+  {
+    "revision": "a7886e3897f6e38ea1f8",
+    "url": "/BestRunner/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "a7886e3897f6e38ea1f8",
+    "url": "/BestRunner/static/js/2.bddb4316.chunk.js"
+  },
+  {
+    "revision": "5bc24cf30240fc9a2ea4fd21c475fd5e",
+    "url": "/BestRunner/static/js/2.bddb4316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b956dc6136745e3e274",
+    "url": "/BestRunner/static/js/main.7122e81e.chunk.js"
+  },
+  {
+    "revision": "149fa6b00ca7a2fccc77",
+    "url": "/BestRunner/static/js/runtime-main.16f5baea.js"
+  }
+]);
